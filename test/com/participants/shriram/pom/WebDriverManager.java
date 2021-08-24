f50a832e2cc679c4eac5f336d910be8a0531f84e
shriram_mogal <shree.s.mogal@gmail.com>
2021-08-24 10:07:35 +0530

    POM implementation

---
+++ test/com/participants/shriram/pom/WebDriverManager.java
@@ -0,0 +1,16 @@
+package pom;
+
+public class WebDriverManager {
+	
+	
+	public static void chromeDriverSetUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "Test/resources/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
